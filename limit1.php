65d41428ee22797d8df37100bd5bdb308c8e0589
srsuper <aot5310@gmail.com>
2020-05-04 11:13:20 +0700

    Update limit1.php

--- limit1.php
+++ limit1.php
@@ -8,3 +8,4 @@ $ln = new KS\Line\LineNotify($token);
 $text = 'ตัวอื่นก็ แทงได้นะจ๊ะ หลังป้ายนี้ แทง 1 ไม่ติด ทุกกรณี '; // Line Notify บังคับให้ใส่ข้อความ แต่อยากส่งแแต่รูปภาพเลยใส่ space ไว้
 $image_path = '/img/limit1.jpg'; //Line notify allow only jpeg and png file
 $ln->send($text, $image_path);
+?>
