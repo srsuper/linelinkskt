e82c1023cea6a349286ba1d3938ac0ff5200b919
srsuper <aot5310@gmail.com>
2020-05-18 02:53:42 +0700

    Update mycon.php

--- im/mycon.php
+++ im/mycon.php
@@ -1,6 +1,6 @@
 
 <?php
-$mysqli = new mysqli("localhost","root","","internat_messenger");
+$mysqli = new mysqli("sql12.freemysqlhosting.net","sql12340596","wpetblKIfN","sql12340596");
 
 // Check connection
 if ($mysqli -> connect_errno) {
